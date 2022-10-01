673767437cb7aa10ee710ad61d933e0f0368357d
aurocheg <aurocheg@gmail.com>
2022-10-01 14:35:23 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <div>Hello world!</div>
+            </header>
+        </div>
+    )
+}
+
+export default App
